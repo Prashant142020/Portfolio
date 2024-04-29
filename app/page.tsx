8e2ba4782a7347f04f5e209bcc9e20ca2b483202
Prashant142020 <prashant142020@gmail.com>
2024-04-30 01:58:32 +0530

    add auth.js and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import { SignIn } from './(auth)/sign-in';
 
 export default function Home() {
   return (
     <main>
       <h1>Hi Prashant</h1>
+      <SignIn />
     </main>
   );
 }
